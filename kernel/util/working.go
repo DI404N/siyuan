4bee610a5536693a29a68986b4c79ab45c47f43f
chenxianan <chenxianan@meituan.com>
2025-03-06 13:13:40 +0800

    feat: unlock vip

--- kernel/util/working.go
+++ kernel/util/working.go
@@ -218,7 +218,7 @@ func SetBooted() {
 
 var (
 	HomeDir, _    = gulu.OS.Home()
-	WorkingDir, _ = os.Getwd()
+	WorkingDir, _ = "../app", ""
 
 	WorkspaceDir       string        // 工作空间目录路径
 	WorkspaceName      string        // 工作空间名称
